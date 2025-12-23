ac395d8515de240747c4ae5c3450ae739a7a55d3
dsemkin <semkin_dmitriy10@vk.com>
2025-12-23 23:03:08 +0400

    aggregator

--- src/main/java/aggregator/SensorPrinterMain.java
+++ src/main/java/aggregator/SensorPrinterMain.java
@@ -1,30 +1,17 @@
 package aggregator;
 
-import common.entities.Device;
-import common.entities.Sensor;
-import common.entities.SensorReading;
-import common.entities.sensor.data.AccelerometerData;
-import common.entities.sensor.data.BarometerData;
-import common.entities.sensor.data.LightData;
-import common.entities.sensor.data.LocationData;
+import common.DevicesAndDeviceSensorsInitializer;
+import common.EntityManagerFactoryProvider;
 import jakarta.persistence.EntityManagerFactory;
-import org.hibernate.cfg.Configuration;
 
 import java.util.Scanner;
 
 public class SensorPrinterMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        EntityManagerFactory factory = new Configuration()
-                .addAnnotatedClass(AccelerometerData.class)
-                .addAnnotatedClass(BarometerData.class)
-                .addAnnotatedClass(LightData.class)
-                .addAnnotatedClass(LocationData.class)
-                .addAnnotatedClass(Device.class)
-                .addAnnotatedClass(Sensor.class)
-                .addAnnotatedClass(SensorReading.class)
-                .buildSessionFactory();
+        EntityManagerFactory factory = EntityManagerFactoryProvider.getFactory();
         try (scanner; factory) {
+            DevicesAndDeviceSensorsInitializer.initialize(factory);
             SensorPrinter sensorPrinter = new SensorPrinter(scanner, factory);
             sensorPrinter.start();
         }
